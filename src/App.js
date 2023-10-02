e0dbd5f738818868d94eab0dfc034a9373c8ffcc
jadaan96 <jadaan.2009@gmail.com>
2023-10-02 16:00:53 +0000

    tour

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Layout from "./layout/index";
 import { Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
-
+import Tour from './pages/Tour/index'
 function App() {
   return (
     <Layout>
       <Routes>
 
-      <Route path='/' element={<HomePage />} />
+      {/* <Route path='/' element={<HomePage />} /> */}
+      <Route path='/' element={<Tour />} />
 
       </Routes>
     </Layout>
