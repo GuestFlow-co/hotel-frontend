df315eae7ba4041b453e2fee7eea76ca6516af44
jadaan96 <jadaan.2009@gmail.com>
2023-10-03 09:27:02 +0000

    fix the routs

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -4,6 +4,7 @@ import RoomList from './Rooms/List/RoomList';
 import { useSelector } from 'react-redux/es';
 import RoomDetail from './Rooms/RoomDetails/RoomDetails';
 import HomePage from './HomePage/HomePage';
+import Tour from './Tour';
 
 export default function Routers() {
   const rooms = useSelector(state => state.rooms.rooms);  
@@ -14,6 +15,7 @@ export default function Routers() {
       <Route path="/rooms/:room_number"  element={<RoomDetail  />} />
       <Route path="/rooms"  element={<RoomList  rooms={rooms}  />} />
       <Route path='/' element={<HomePage />} />
+      <Route path='/tour' element={<Tour />} />
 
     </Routes>
   );
