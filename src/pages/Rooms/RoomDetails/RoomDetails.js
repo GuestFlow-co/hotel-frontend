a0db66172c80d960c407294953d857eab36d6f45
Ayah-AQ <ayoya_1998@hotmail.com>
2023-10-09 09:54:18 +0000

    edit

--- src/pages/Rooms/RoomDetails/RoomDetails.js
+++ src/pages/Rooms/RoomDetails/RoomDetails.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { Box, Image, Flex, Button } from '@chakra-ui/react';
-
+import { Box, Image, Flex, Button, Icon } from '@chakra-ui/react';
 import { DetailWrapper, BackButton } from "./style";
-import { Carousel, Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { BsFillPeopleFill } from 'react-icons/bs';
+// import { IoBedOutline } from 'react-icons/io';
 import Rating from "../Rate/Rate";
-
-import hero from "../../../assets/Rooms/5655441.jpg"
-import RoomHero from "../RoomHero/RoomHero";
+import hero from "../../../assets/Rooms/5655441.jpg";
 
 const RoomDetail = () => {
   const { room_number } = useParams();
   const [currentIndex, setCurrentIndex] = useState(0);
-  
-  const rooms = useSelector((state) => state.rooms.rooms);
-  const images= rooms.photoUrl
 
+  const rooms = useSelector((state) => state.rooms.rooms);
+
+  // Find the room based on room_number
   const room = rooms.find((room) => room.room_number === room_number);
 
+  if (!room) {
+    return <p>Room not found</p>;
+  }
+
+  const { roomType, room_capacity, roomPrice, photoUrl,bed_nums,Room_space, } = room;
+  const images = photoUrl || [];
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -30,50 +35,66 @@ const RoomDetail = () => {
     );
   };
 
-  if (!room) {
-    return <p>Room not found</p>;
-  }
-
   return (
-    <DetailWrapper>
-           <Box position="relative" w="70%" height="150px" paddingTop={20}>
-      <Image
-        src={images[currentIndex]}
-        alt={`Image ${currentIndex}`}
-        objectFit="cover"
-        w="100%"
-        h="100%"
+    <>
+      <img
+        src={hero}
+        alt={`Room Image`}
+        style={{ width: "100%", height: "300px", objectFit: "cover" }}
       />
-      <Flex
-        justify="space-between"
-        position="absolute"
-        w="100%"
-        bottom="1rem"
-        p="1rem"
-      >
-        <Button onClick={prevImage} colorScheme="teal" variant="outline">
-          Previous
-        </Button>
-        <Button onClick={nextImage} colorScheme="teal" variant="outline">
-          Next
-        </Button>
-      </Flex>
-    </Box>
-        <Container>
-        
-      <h1> {room.roomType}</h1>
-      <p>Capacity: {room.room_capacity}</p>
-      <p>Rate: {room.theRoomRate}</p>
-      <p>Status: {room.roomStatus}</p>
-      <p className="room-price">Price: {room.roomPrice} JD</p>
-      </Container>
-      <Rating room={room}/>
-      <Link to="/rooms">
-        <BackButton type="back" className="btn btn-success" color="blue">
-          Back
-        </BackButton>
-      </Link>
-    </DetailWrapper>
+      <DetailWrapper style={{ backgroundColor: 'rgba(223, 215, 191, 0.26)', width: "70%", padding: "5px" }}>
+        <Box position="relative" w="100%" paddingLeft={5}>
+          <Image
+            src={images[currentIndex]}
+            alt={`Image ${currentIndex}`}
+            w="97%"
+            h="400px"
+          />
+          <Flex
+            justify="space-between"
+            position="absolute"
+            w="90%"
+            bottom="1rem"
+            p="1rem"
+          >
+            <Button onClick={prevImage} colorScheme="teal" variant="outline" fontWeight={1000} color={"white"}>
+              Previous
+            </Button>
+            <Button onClick={nextImage} colorScheme="teal" variant="outline" fontWeight={1000} color={"white"}>
+              Next
+            </Button>
+          </Flex>
+        </Box>
+        <Container style={{ paddingTop: "2%" }}>
+          <Flex justifyContent={"space-between"}>
+            <h3>{roomType}</h3>
+            <h3 className="m-0 room-price"> From {roomPrice} JD/Night</h3>
+          </Flex>
+          <br/>
+          <p>Features</p>
+          <hr style={{ borderColor: "brown", borderWidth: "1px", margin: "10px 0" }} />
+          <div style={{ display: "flex" }}>
+            {/* <Icon as={IoBedOutline} mr={5} /> */}
+            <p style={{ color: "black" }}>{room_capacity} person</p>
+          </div>
+
+          <div style={{ display: "flex" }}>
+            <Icon as={BsFillPeopleFill} mr={5} />
+            <div>
+            <p style={{ color: "black" }}>{bed_nums} person</p>
+            </div>
+          </div>
+        </Container>
+        {/* <Rating room={room} /> */}
+        <br/>
+        <Link to="/rooms">
+          {/* <BackButton type="back" className="btn btn-success" color="blue">
+            Back
+          </BackButton> */}
+        </Link>
+        <br/>
+      </DetailWrapper>
+    </>
   );
 };
 
