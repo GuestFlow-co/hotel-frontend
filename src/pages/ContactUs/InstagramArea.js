e06807422d15d0417ff5b3bb86b1ef1486d223d2
FarrahYasin <farah.y.yassin@gmail.com>
2023-10-10 23:23:23 +0000

    contact us page

---
+++ src/pages/ContactUs/InstagramArea.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function InstagramArea() {
+  return (
+    <section className="instagram-area">
+      <div className="instagram-item wow fadeInUp delay-0-2s">
+        <a className="instagram-gallery" href="assets/images/instagrams/instagram1.jpg">
+          <img src="https://rahalar.com/wp-content/uploads/2022/03/%D8%A7%D9%81%D8%B6%D9%84-%D9%81%D9%86%D8%A7%D8%AF%D9%82-%D8%AA%D9%82%D8%B3%D9%8A%D9%85-%D9%84%D9%84%D8%B9%D9%88%D8%A7%D8%A6%D9%84-1024x682.jpg" alt="instagram" />
+        </a>
+      </div>
+      <div className="instagram-item wow fadeInUp delay-0-3s">
+        <a className="instagram-gallery" href="assets/images/instagrams/instagram2.jpg">
+          <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/21/81/23/superior-one-bedroom.jpg?w=700&h=-1&s=1" alt="instagram" />
+        </a>
+      </div>
+      <div className="instagram-item wow fadeInUp delay-0-4s">
+        <div className="content text-white">
+          <div className="icon"><i className="fab fa-instagram"></i></div>
+          <h2><a href="https://www.instagram.com">Follow Our Instagram</a></h2>
+          <hr />
+          <div className="call-text">Make A Call</div>
+          <a className="h2" href="callto:+962(123)45688">+962 (123) 456 88</a>
+        </div>
+      </div>
+      <div className="instagram-item wow fadeInUp delay-0-5s">
+        <a className="instagram-gallery" href="">
+          <img src="https://yallabook.com/blog/admincp/userfiles/%D9%81%D9%86%D8%AF%D9%82%20%D9%88%D8%B4%D9%82%D9%82%20%D8%B1%D8%A7%D8%AF%D9%8A%D8%B3%D9%88%D9%86%20%D8%A7%D9%84%D8%AF%D9%85%D8%A7%D9%85%20%D8%A7%D9%84%D9%85%D8%AF%D9%8A%D9%86%D8%A9%20%D8%A7%D9%84%D8%B5%D9%86%D8%A7%D8%B9%D9%8A%D8%A9.jpg" alt="instagram" />
+        </a>
+      </div>
+      <div className="instagram-item wow fadeInUp delay-0-6s">
+        <a className="instagram-gallery" href="https://firasf.com/wp-content/uploads/2020/09/Hotel-Design-Ideas-1.jpg">
+          <img src="https://firasf.com/wp-content/uploads/2020/09/Hotel-Design-Ideas-1.jpg" alt="instagram" />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default InstagramArea;
