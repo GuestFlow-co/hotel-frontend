578632c402e1f8ae2c458a0c873227b66f44b4fc
FarrahYasin <farah.y.yassin@gmail.com>
2023-10-10 06:21:09 +0000

    edit sign up

---
+++ src/pages/Auth/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./ForgotPassword.scss";
+
+function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/forgotPassword`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }), // Send the email to the backend
+        }
+      );
+
+      if (response.ok) {
+        // Successful email submission
+        setSuccessMessage("Password reset email sent successfully.");
+      } else {
+        const data = await response.json();
+        setError(data.error || "Error: Something went wrong.");
+      }
+    } catch (error) {
+      setError("Error: User not found or internal server error.");
+    }
+  };
+
+  return (
+    <div className="password-main-container">
+      <div className="password-forget-form">
+        <h1 className="forget-h1">Forgot Password</h1>
+        {error && <div className="forget-error">{error}</div>}
+        {successMessage && (
+          <div className="forget-success">{successMessage}</div>
+        )}
+        <form className="forget-form" onSubmit={handleSubmit}>
+          <div className="forget-form-group">
+            <div className="forget-input">
+              <input
+                type="email"
+                // placeholder="Enter your email"
+                placeholder=""
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label for="email" className="forget-label" htmlFor="email">
+                Enter your email
+              </label>
+            </div>
+          </div>
+          <div className="forget-button-container">
+            <button className="forget-button" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPassword;
